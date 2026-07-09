6047c1797496bd5f3beac6a8ae51dcbc377e83fe
Teinishi <79356047+Teinishi@users.noreply.github.com>
2026-07-09 23:13:39 +0900

    vueuse を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxtjs/i18n', '@tresjs/nuxt'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxtjs/i18n',
+    '@tresjs/nuxt',
+    '@vueuse/nuxt',
+  ],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
