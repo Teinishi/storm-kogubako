94321010484cb970b528127843ae7c5482b45924
Teinishi <79356047+Teinishi@users.noreply.github.com>
2026-07-24 22:37:58 +0900

    fov を変更

--- app/components/MeshViewerCanvas.client.vue
+++ app/components/MeshViewerCanvas.client.vue
@@ -84,7 +84,7 @@ function createObjectUniforms(item: DeepReadonly<MeshViewerCanvasItem>): Stormwo
     class="h-full w-full"
   >
     <!-- @vue-expect-error -->
-    <TresPerspectiveCamera :position="[-4, 0, 2]" />
+    <TresPerspectiveCamera :position="[-4, 0, 2]" :fov="70" />
     <OrbitControls :enable-damping="false" :mouse-buttons="orbitMouseButtons" />
     <primitive :object="lights" />
     <!-- @vue-expect-error -->
